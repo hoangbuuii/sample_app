9dcf24b4763cc19c7b6549006bbcb45fc3f7858b
Bui Huy Hoang <hoangbui237@gmail.com>
2017-10-18 11:45:06 +0700

    Finish user signup

--- db/migrate/20171013013309_create_users.rb
+++ db/migrate/20171013013309_create_users.rb
@@ -6,7 +6,7 @@ class CreateUsers < ActiveRecord::Migration[5.1]
 
       t.timestamps
     end
-      add_index :users, :email, unique: true
-  	  add_column :users, :password_digest, :string
+    add_index :users, :email, unique: true
+    add_column :users, :password_digest, :string
   end
 end
