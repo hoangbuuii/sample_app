f5e5ff1edea40860464a74fcc569ba4d845d1d79
Bui Huy Hoang <hoangbui237@gmail.com>
2017-10-28 10:43:03 +0700

    Add password reset

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,9 +15,14 @@ class SessionsController < ApplicationController
         redirect_to root_url
       end
     else
-      flash[:danger] = t ".invalid_log_in"
+      flash.now[:danger] = t ".invalid_log_in"
       render :new
-    end
+    end 
+  end
+
+  def destroy
+    log_out
+    redirect_to root_url
   end
 
   def destroy
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -17,6 +17,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   config.action_mailer.delivery_method = :test
+
   config.action_mailer.default_url_options = {host: "localhost:3000"}
 
   config.active_support.deprecation = :stderr
